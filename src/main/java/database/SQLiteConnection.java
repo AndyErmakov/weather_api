f2e562e8cbd5ba11e2088992a4ae0b22579394cd
AndyErmakov <an.ermakov1@gmail.com>
2025-02-19 23:49:27 +0200

    3d commit with User+City select

--- src/main/java/database/SQLiteConnection.java
+++ src/main/java/database/SQLiteConnection.java
@@ -1,19 +1,20 @@
 package database;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.List;
-
 import model.City;
 import model.User;
 
+
+import java.sql.*;
+import java.util.ArrayList;
+
+
+
 public class SQLiteConnection {
-	static String url = 
+	private static String url = 
 		"jdbc:sqlite:/Users/apple/eclipse-workspace/weather_api/src/main/java/database/cities_db.db";
+	private static ArrayList<User> usersList;	
 	
+
 //	Adding User object to the database		
 	public static void DB_Add_City (User user) throws SQLException {
 //			Make (Get) connection to the Database 
@@ -47,6 +48,56 @@ public class SQLiteConnection {
 				}
 			}
 		}
+	
+	public static ArrayList<User> DB_SelectUsersCities () throws SQLException {
+//		Make (Get) connection to the Database 
+		try (Connection conn = DriverManager.getConnection(url)) {
+			boolean isValid = conn.isValid(0);
+			System.out.println("Do we have a valid db connection? = " + isValid);		
+//		        select user from database to check if it already exist
+				usersList = new ArrayList<User>();
+		        PreparedStatement selectStatement = conn.prepareStatement
+		        	("select distinct UserName from Favorite_Cities ");
+//		        this will return a ResultSet
+		        ResultSet rs = selectStatement.executeQuery();
+
+//		       Add data to the ArrayLise
+		        while (rs.next()) {
+		        	String name = rs.getString("UserName");
+		        	
+		            User user = new User(name);
+		            usersList.add(user);		            
+		        }
+		        
+		        for (User user1 : usersList ) {
+			        System.out.println("\n"+user1.getUsername());  //Print for testing - delete after debugging
+			        
+			        PreparedStatement selectStatement1 = conn.prepareStatement
+				        	("select * from Favorite_Cities where UserName =  ?");
+			        selectStatement1.setString(1, user1.getUsername());
+	//			        this will return a ResultSet
+				        ResultSet rs1 = selectStatement1.executeQuery();
+	
+	//			       Add data to the ArrayList
+				        while (rs1.next()) {
+				        	String cityName = rs1.getString("CityName");
+				        	Double lattitude = rs1.getDouble("Lattitude");
+				        	Double longtitude = rs1.getDouble("Longtitude");
+				        	City city = new City(cityName,lattitude, longtitude);
+				            user1.addCity(city);
+				            System.out.print		//Print for testing - delete after debugging
+				            (city.getName()+"\t"+ city.getLattitude()+"\t"+ city.getLongtitude()+ "\n");
+				        }
+		        }
+		}
+		System.out.println("\nUsers List"); //Print for testing - delete after debugging
+		for (User user2 : usersList) {   //Print for testing - delete after debugging
+			System.out.println("\n"+ user2.getUsername());  //Print for testing - delete after debugging
+			System.out.print("\n"+ user2.getCityList());  //Print for testing - delete after debugging
+		}
+		return usersList;
+	}
+
 
 	public static void main(String[] args) throws SQLException {
 		// TODO Auto-generated method stub
@@ -57,7 +108,8 @@ public class SQLiteConnection {
 		user1.addCity(city1);
 		user1.addCity(city2);
 		user1.addCity(city3);
-		DB_Add_City(user1);
+//		DB_Add_City(user1);
+		DB_SelectUsersCities();
 
 	}
 
